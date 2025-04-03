7ea35be027a0ea0f051e3a5407946479b0f6db55
german-spb <german@pushkin-spb.com>
2025-04-03 17:07:46 +0300

    first commit

---
+++ resurses/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from .models import *
+
+def home(request):
+    return render(request, 'home.html')
